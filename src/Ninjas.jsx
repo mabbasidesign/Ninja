f125305ec3db52c18fc2580f33159e3a87819d9f
unknown <mabbasidesign@yahoo.com>
2018-11-15 15:12:49 -0700

    sfc

--- src/Ninjas.jsx
+++ src/Ninjas.jsx
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 
-class Ninjas extends Component {
-    state = {  }
-    render() {
-        const { ninjas } = this.props;
-        const ninjaList = ninjas.map( ninja => {
-            return(
-                <div className="ninja">
-                    <div>Name: {ninja.name}</div>
-                    <div>Age: {ninja.age}</div>
-                    <div>Belt: {ninja.belt}</div>
-                </div>
-            )
-        });
-
-        return ( 
-            <div className="ninja-list">
-                { ninjaList }
+const Ninjas = (ninjas) => {
+    const ninjaList = ninjas.map( ninja => {
+        return(
+            <div key={ninja.id} className="ninja">
+                <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
             </div>
-         );
-    }
+        )
+    });
+
+    return ( 
+        <div className="ninja-list">
+            { ninjaList }
+        </div>
+        );
 }
 
 export default Ninjas;
